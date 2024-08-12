c26615fec8136ff6e99edf616ae68165248f8104
Amit Mishra <amitmishra25102@gmail.com>
2024-08-12 11:48:54 +0530

    updated origin

--- server/index.js
+++ server/index.js
@@ -17,7 +17,11 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001"],
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:3001",
+      "https://taskodo.netlify.app",
+    ],
     methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
